076b6df506b6a253c5b7573378de1e77d58408c6
Stefan Cova <stefan.cova@publicissapient.com>
2020-01-24 12:11:23 +0100

    update api url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Book, Offers } from '../models';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Book, Offers } from "../models";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ApiService {
-  private url = 'http://henri-potier.xebia.fr/books';
+  //private url = 'http://henri-potier.xebia.fr/books';
+  private url = "https://henri-potier-proxy.herokuapp.com/books";
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Book[]> {
@@ -16,11 +17,11 @@ export class ApiService {
 
   getBestOffer(refsUrls: string): Observable<Offers> {
     console.log(
-      '#refsUrls : ',
-      this.url + '/' + refsUrls + '/commercialOffers'
+      "#refsUrls : ",
+      this.url + "/" + refsUrls + "/commercialOffers"
     );
     return this.http.get<Offers>(
-      this.url + '/' + refsUrls + '/commercialOffers'
+      this.url + "/" + refsUrls + "/commercialOffers"
     );
   }
 }
